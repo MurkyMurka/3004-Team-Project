cd4853c0d4eae3530c3a183cf208f61e5b34343d
Ludg3r <84548375+Ludg3r@users.noreply.github.com>
2025-03-30 17:25:41 -0400

    Add files via upload

---
+++ COMP3004_Final/UserProfile.cpp
@@ -0,0 +1,31 @@
+#include "UserProfile.h"
+
+userProfile::userProfile(float i, float cor, float tar, float curr, float io, float immBol) {
+    icr = i;
+    correctionFactor = cor;
+    targetBG = tar;
+    currBG = curr;
+    iob = io;
+    immediateBolus = immBol;
+    isActive = false;
+}
+
+void userProfile::addTimeSegment(double t) {
+    timeSegments.append(t);
+}
+
+void userProfile::activateProfile() { isActive = true; }
+
+void userProfile::setIcr(float newNum) { icr = newNum; }
+
+void userProfile::setCorrectionFactor(float newNum) { correctionFactor = newNum; }
+
+void userProfile::setTargetBG(float newNum) { targetBG = newNum; }
+
+void userProfile::setCurrBG(float newNum) { currBG = newNum; }
+
+void userProfile::setIob(float newNum) { iob = newNum; }
+
+void userProfile::setImmediate(float newNum) { immediateBolus = newNum; }
+
+bool userProfile::isActive() { return isActive; }
